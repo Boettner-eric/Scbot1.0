ce3d0588300782ca948890f62135038ef7cb11ba
Eric Boettner <ericboettner@Erics-MacBook-Pro.local>
2017-01-23 20:22:12 -0800

    Initial Commit

---
+++ test_Scbot1.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_Scbot1.0
+//
+//  Created by Eric Boettner on 1/23/17.
+//  Copyright © 2017 Eric. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
